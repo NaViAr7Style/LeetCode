a449df39efc7be6f4b3b001f8de2c676d194199a
NaViAr7Style <ar7styleblizz@gmail.com>
2025-01-05 19:51:41 +0200

    Removed code from Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
-import Week1.ValidParentheses;
-
 public class Main {
   public static void main(String[] args) {
-
-    ValidParentheses vp = new ValidParentheses();
-
-    System.out.println(vp.isValid("()"));
-    System.out.println(vp.isValid("()[]{}"));
-    System.out.println(vp.isValid("(]"));
-    System.out.println(vp.isValid("([])"));
 
   }
 }
